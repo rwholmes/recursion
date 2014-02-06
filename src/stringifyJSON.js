2bd64c2d37a40a0e6cdfe4534d255292c4244309
Robert Holmes <rwholmes1@gmail.com>
2014-02-06 10:58:05 -0800

    Stringify function that works for objects and arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,18 +5,50 @@
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
 	// Case for simple non-object input or null
-	if (typeof obj !== "object" || obj === null) {
+	if (typeof obj !== 'object' || obj === null) {
 		// Case for if string
-  		if (typeof obj === "string") {
+  		if (typeof obj === 'string') {
   			obj = '"' + obj + '"';
   		}
-  		else if (typeof obj === "number" || obj === null) {
+  		else {
   			obj = '' + obj;
   		}
+  		return obj;
 	}
 	// Case for input is an object
 	else {
-
+		var objContents = [];
+		var val;
+		var type;
+		for (var key in obj) {
+			// Going to use recursion to call the top part if the val is a string/num
+			val = obj[key];
+			type = typeof val;
+			// Case for value is a string
+			if (type === 'string') {
+				val = '"' + val + '"';
+			}
+			// If the value is an object we need to stringify that object using recursion
+			else if (type === 'object') {
+				val = stringifyJSON(val);
+			}
+			if (Array.isArray(obj)) {
+				objContents.push(val);
+			}
+			else {
+				objContents.push('"' + key + '":' + val);
+			}
+		}
+		if (Array.isArray(obj)) {
+			var result = '[' + objContents + ']';
+		}
+		else {
+			var result = '{' + objContents + '}';
+		}
+		return result;
 	}
-  	return obj;
 };
+
+
+
+
