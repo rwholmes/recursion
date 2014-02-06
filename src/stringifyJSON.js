0e0c43e87e45872395bc6759e1f2ad43ea9f6a3a
Robert Holmes <rwholmes1@gmail.com>
2014-02-06 11:25:32 -0800

    Cleaned up stringify code removing unnecessary declarations

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,6 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
-
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
 	if (obj === undefined) {
@@ -22,11 +21,9 @@ var stringifyJSON = function (obj) {
 	else {
 		var objContents = [];
 		var val;
-		var type;
 		for (var key in obj) {
 			// Going to use recursion to call the top part if the val is a string/num
 			val = obj[key];
-			type = typeof val;
 			// Recursively call stringify on value
 			val = stringifyJSON(val);
 			if (Array.isArray(obj)) {
