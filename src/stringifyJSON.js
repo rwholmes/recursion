04f7911dddd650cef17fe00dd90f8039dd9b9567
Robert Holmes <rwholmes1@gmail.com>
2014-02-06 10:08:39 -0800

    Impletented stringify function for simple inputs, numbers and strings

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,22 @@
 // this is what you would do if you liked things to be easy:
 // var stringifyJSON = JSON.stringify;
 
+
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+	// Case for simple non-object input or null
+	if (typeof obj !== "object" || obj === null) {
+		// Case for if string
+  		if (typeof obj === "string") {
+  			obj = '"' + obj + '"';
+  		}
+  		else if (typeof obj === "number" || obj === null) {
+  			obj = '' + obj;
+  		}
+	}
+	// Case for input is an object
+	else {
+
+	}
+  	return obj;
 };
